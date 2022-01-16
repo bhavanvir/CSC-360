ec93e2c3b2b5fa1fc424409aad0c4dbb44bff39b
Bhavanvir Rai <bhavanvir.r@gmail.com>
2022-01-16 13:22:30 -0800

    Add files via upload

--- main.c
+++ main.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 
-void fetch_info(){
-	char buffer[1024];
+
+void fetch_info(char *buffer){
 	buffer[0] = '\0';
 
 	char *login = getlogin();
@@ -22,7 +27,24 @@ void fetch_info(){
 	strcat(buffer, " > ");	
 }
 
+void exec_cmd(char **cmd){
+	pid_t pid = fork();
+
+	if(pid == -1)
+		perror("pid");
+	else if(pid > 0)
+		wait(NULL);
+	else{
+		if(execvp(cmd[0], cmd) == -1)
+			perror("execvp");
+	}
+
+}
+
 int main(){
-	fetch_info();
+	char buffer[1024];
+	fetch_info(buffer);
+	char *cmd = readline(buffer);
+
 	return 0;
 }
