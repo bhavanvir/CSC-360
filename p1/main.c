3cac62adf98547465a281ca8b9c7d0c7ff46bc6d
Bhavanvir Rai <bhavanvir.r@gmail.com>
2022-02-06 15:11:22 -0800

    Add files via upload

--- p1/main.c
+++ p1/main.c
@@ -13,10 +13,6 @@ typedef struct node_t {
 	struct node_t *next;
 } node_t;
 
-node_t *head = NULL;
-
-int num_pro = 0;
-
 void fetch_info(char *buffer){
 	char hostname[256];
        	gethostname(hostname, sizeof(hostname));	
@@ -32,7 +28,7 @@ void fetch_info(char *buffer){
 	strcat(buffer, " > ");
 }
 
-void exec_cmd(char **tokenized){
+void execute_cmd(char **tokenized){
 	pid_t pid = fork();
 
 	if(pid < 0)
@@ -79,7 +75,19 @@ void change_dir(char **tokenized, int num_cmd){
 		chdir(tokenized[1]);
 }
 
-void background_pro(char **tokenized){
+int total_pro(node_t **head){
+	node_t* curr = *head;
+
+	int total = 0;
+	while(curr != NULL){
+		total++;
+		curr = curr->next;
+	}
+
+	return total;
+}
+
+void background_pro(char **tokenized, node_t **head){
 	pid_t pid = fork();
 
 	if(pid < 0)
@@ -88,51 +96,54 @@ void background_pro(char **tokenized){
 		execvp(tokenized[1], tokenized + 1);
 	else{
 		node_t* new_node = (node_t*)malloc(sizeof(node_t));
-
-		new_node->pid = pid;
 		
+		if(new_node == NULL) perror("malloc");
+
+		new_node->cmd[0] = '\0';
+		new_node->pid = pid;
+
 		int i = 0;
 		while(tokenized[i] != NULL){
 			if(strcmp(tokenized[i], "bg") != 0){
-				strcat(new_node->cmd, " ");
 				strcat(new_node->cmd, tokenized[i]);
+				strcat(new_node->cmd, " ");
 			}
 			i++;
 		}
-		new_node->next = head;
-		head = new_node;
-		num_pro++;
+		new_node->next = * head;
+		*head = new_node;
 	}
 		
 }
 
-void background_list(){
-	node_t* curr = head;
+void background_list(node_t **head){
+	node_t* curr = *head;
 
 	while(curr != NULL){
-		printf("%d: %s", curr->pid, curr->cmd);
+		printf("%d: %s\n", curr->pid, curr->cmd);
 		curr = curr->next;
 	}
-	printf("Total background jobs: %d", num_pro);
+	printf("Total Background jobs: %d\n", total_pro(head));
 }
 
-void terminate_pro(){
-	node_t* curr = head;
+void check_pro(node_t **head){
+	node_t* curr = *head;
 	
-	if(num_pro > 0){
+	if(total_pro(&curr) > 0){
 		pid_t ter = waitpid(0, NULL, WNOHANG);
-		
+
 		while(ter > 0){
-			if(curr->pid == ter){
-				printf("%d: %s has terminated", curr->pid, curr->cmd);
-				curr = curr->next;
-			}else{
-				while(curr->next->pid != ter)
+			if(ter > 0){
+				if(curr->pid == ter){
+					printf("%d: %s has terminated\n", curr->pid, curr->cmd);
 					curr = curr->next;
-				printf("%d: %s has terminated", curr->next->pid, curr->next->cmd);
-				curr->next = curr->next->next;
+				}else{
+					while(curr->next->pid != ter)
+						curr = curr->next;
+					printf("%d: %s has terminated\n", curr->pid, curr->cmd);
+					curr->next = curr->next->next;
+				}
 			}
-			num_pro--;
 			ter = waitpid(0, NULL, WNOHANG);
 		}
 	}
@@ -150,21 +161,21 @@ int main(){
 		int num_cmd = 0;
 		char **tokenized = tokenize_str(cmd, &num_cmd);
 		
+		node_t *head;
+		check_pro(&head);
 
 		if(num_cmd == 0) continue;
 
 		if(strcmp(tokenized[0], "cd") == 0)
 			change_dir(tokenized, num_cmd);	
 		else if(strcmp(tokenized[0], "bg") == 0)
-			background_pro(tokenized);
+			background_pro(tokenized, &head);
 		else if(strcmp(tokenized[0], "bglist") == 0)
-			background_list();
+			background_list(&head);
 		else if(strcmp(tokenized[0], "exit") == 0)
 			exit(1);
 		else 
-			exec_cmd(tokenized);
-
-		terminate_pro();
+			execute_cmd(tokenized);
 	}
 
 	return 0;
