eeeb2a6f12c6646d8fb299982bd2444e1ca57065
Bhavanvir Rai <bhavanvir.r@gmail.com>
2022-02-08 09:59:59 -0800

    Add files via upload

--- p1/main.c
+++ p1/main.c
@@ -7,12 +7,14 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
+//Adapted from the tutorial slides: linked list structure containing the pid, command and next node 
 typedef struct node_t {
 	pid_t pid;
 	char cmd[1024];
 	struct node_t *next;
 } node_t;
 
+//Count the total number of background processes by iterating on the non-null nodes of the linked list
 int total_pro(node_t **head){
 	node_t* curr = *head;
 
@@ -25,35 +27,39 @@ int total_pro(node_t **head){
 	return total;
 }
 
+//Concatenate the background process commands to each node in the linked list if the process is a parent
 void background_pro(char **tokenized, node_t **head){
 	pid_t pid = fork();
 
 	if(pid < 0)
-		perror("fork");
+		perror("error");
 	else if(pid == 0)
 		execvp(tokenized[1], tokenized + 1);
 	else{
+		//Allocate memory for the new node that will be added to the linked list
 		node_t* new_node = (node_t*)malloc(sizeof(node_t));
 		
-		if(new_node == NULL) perror("malloc");
+		//Check to see if the new node was successfully created
+		if(new_node == NULL) perror("error");
 
 		new_node->cmd[0] = '\0';
 		new_node->pid = pid;
-
-		int i = 0;
-		while(tokenized[i] != NULL){
+		
+		for(int i = 0; tokenized[i] != NULL; i++){
+			//Skip the first command in the tokenized input and concatenate the rest
 			if(strcmp(tokenized[i], "bg") != 0){
 				strcat(new_node->cmd, tokenized[i]);
 				strcat(new_node->cmd, " ");
 			}
-			i++;
 		}
-		new_node->next = * head;
+
+		new_node->next = *head;
 		*head = new_node;
 	}
 		
 }
 
+//Print the total number of background jobs by iterating on the non-null elements of the linked list 
 void background_list(node_t **head){
 	node_t* curr = *head;
 
@@ -65,6 +71,7 @@ void background_list(node_t **head){
 	printf("Total Background jobs: %d\n", total_pro(head));
 }
 
+//Adapted from the tutorial slides: check to see if there are any child processes still executing in the background
 void check_pro(node_t **head){
 	node_t* curr = *head;
 	
@@ -88,6 +95,7 @@ void check_pro(node_t **head){
 	}
 }
 
+//Change directories by using the second element in the tokenized character array
 void change_dir(char **tokenized, int num_cmd){
 	if(tokenized[1] == NULL || strcmp(tokenized[1], "~") == 0)
 		chdir(getenv("HOME"));
@@ -99,6 +107,7 @@ void change_dir(char **tokenized, int num_cmd){
 		chdir(tokenized[1]);
 }
 
+//Execute commands that aren't directly supported in the main function
 void execute_cmd(char **tokenized){
 	pid_t pid = fork();
 
@@ -107,21 +116,26 @@ void execute_cmd(char **tokenized){
 	else if(pid > 0)
 		wait(NULL);
 	else
-		if(execvp(tokenized[0], tokenized) == -1) perror("execvp");	
+		if(execvp(tokenized[0], tokenized) == -1) perror("error");	
 }
 
+//Tokenize the user input, and return a dynamically allocated character array
 char **tokenize_str(char *cmd, int *num_cmd){
 	size_t init_size = 2;
+
+	//Tokenize the input commands using an empty space as the delimiter 
 	char *t = strtok(cmd, " ");
 	char **t_cmd = malloc(sizeof(char*));
 
 	int i = 0;
 	while(t != NULL){
-		if( i >= init_size){
+		if(i >= init_size){
+			//Allocate a temporary charater twice the inital size, in case the original array ran out of space
 			init_size *= 2;
 			char **tmp = (char **)realloc(t_cmd, init_size * sizeof(char *));
-
-			if(tmp == NULL) perror("realloc");
+			
+			//Check to see if the temporary array was successfully created
+			if(tmp == NULL) perror("error");
 
 			t_cmd = tmp;
 		}
@@ -129,12 +143,14 @@ char **tokenize_str(char *cmd, int *num_cmd){
 		t = strtok(NULL, " ");
 
 	}
+	//Pointer with the value of the total number of tokenized commands
 	*num_cmd = i;
 	t_cmd[i] = NULL;	
 	
 	return t_cmd;
 }
 
+//Concatenate the login, hostname and current working directory in a single string
 void fetch_info(char *buffer){
 	char hostname[1024];
        gethostname(hostname, sizeof(hostname));	
@@ -162,11 +178,14 @@ int main(){
 		int num_cmd = 0;
 		char **tokenized = tokenize_str(cmd, &num_cmd);
 		
+		//Initialize the linked list structure by declaring the head node
 		node_t *head;
 		check_pro(&head);
-
+		
+		//If the user enters no command, i.e. just pressing 'Enter', go to the next loop iteration  
 		if(num_cmd == 0) 
 			continue;
+		//Execute each command by comparing the first element of the tokenized array with the supported command
 		else if(strcmp(tokenized[0], "cd") == 0)
 			change_dir(tokenized, num_cmd);	
 		else if(strcmp(tokenized[0], "bg") == 0)
@@ -178,5 +197,6 @@ int main(){
 		else 
 			execute_cmd(tokenized);
 	}
+
 	return 0;
 }
