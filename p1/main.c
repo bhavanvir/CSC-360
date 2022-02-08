aef58b17588316ec035e2d7005bd1e8ca813a60a
Bhavanvir Rai <bhavanvir.r@gmail.com>
2022-02-07 21:53:29 -0800

    Add files via upload

--- p1/main.c
+++ p1/main.c
@@ -13,68 +13,6 @@ typedef struct node_t {
 	struct node_t *next;
 } node_t;
 
-void fetch_info(char *buffer){
-	char hostname[256];
-       	gethostname(hostname, sizeof(hostname));	
-		
-	char curr_dir[256];
-	getcwd(curr_dir, sizeof(curr_dir));
-
-	strcat(buffer, getlogin());
-	strcat(buffer, "@");
-	strcat(buffer, hostname);
-	strcat(buffer, ": ");
-	strcat(buffer, curr_dir);
-	strcat(buffer, " > ");
-}
-
-void execute_cmd(char **tokenized){
-	pid_t pid = fork();
-
-	if(pid < 0)
-		perror("pid");
-	else if(pid > 0)
-		wait(NULL);
-	else
-		if(execvp(tokenized[0], tokenized) == -1) perror("execvp");	
-}
-
-char **tokenize_str(char *cmd, int *num_cmd){
-	size_t init_size = 2;
-	char *t = strtok(cmd, " ");
-	char **t_cmd = malloc(sizeof(char*));
-
-	int i = 0;
-	while(t != NULL){
-		if( i >= init_size){
-			init_size *= 2;
-			char **tmp = (char **)realloc(t_cmd, init_size * sizeof(char *));
-
-			if(tmp == NULL) perror("realloc");
-
-			t_cmd = tmp;
-		}
-		t_cmd[i++] = t;
-		t = strtok(NULL, " ");
-
-	}
-	*num_cmd = i;
-	t_cmd[i] = NULL;	
-	
-	return t_cmd;
-}
-
-void change_dir(char **tokenized, int num_cmd){
-	if(tokenized[1] == NULL || strcmp(tokenized[1], "~") == 0)
-		chdir(getenv("HOME"));
-	else if(strcmp(tokenized[1], "../") == 0 || strcmp(tokenized[1], "..") == 0)
-		chdir("..");
-	else if(num_cmd > 2 && tokenized[2] != NULL)
-		perror("chdir");
-	else
-		chdir(tokenized[1]);
-}
-
 int total_pro(node_t **head){
 	node_t* curr = *head;
 
@@ -123,6 +61,7 @@ void background_list(node_t **head){
 		printf("%d: %s\n", curr->pid, curr->cmd);
 		curr = curr->next;
 	}
+	
 	printf("Total Background jobs: %d\n", total_pro(head));
 }
 
@@ -149,6 +88,68 @@ void check_pro(node_t **head){
 	}
 }
 
+void change_dir(char **tokenized, int num_cmd){
+	if(tokenized[1] == NULL || strcmp(tokenized[1], "~") == 0)
+		chdir(getenv("HOME"));
+	else if(strcmp(tokenized[1], "../") == 0 || strcmp(tokenized[1], "..") == 0)
+		chdir("..");
+	else if(num_cmd > 2 && tokenized[2] != NULL)
+		perror("chdir");
+	else
+		chdir(tokenized[1]);
+}
+
+void execute_cmd(char **tokenized){
+	pid_t pid = fork();
+
+	if(pid < 0)
+		perror("fork");
+	else if(pid > 0)
+		wait(NULL);
+	else
+		if(execvp(tokenized[0], tokenized) == -1) perror("execvp");	
+}
+
+char **tokenize_str(char *cmd, int *num_cmd){
+	size_t init_size = 2;
+	char *t = strtok(cmd, " ");
+	char **t_cmd = malloc(sizeof(char*));
+
+	int i = 0;
+	while(t != NULL){
+		if( i >= init_size){
+			init_size *= 2;
+			char **tmp = (char **)realloc(t_cmd, init_size * sizeof(char *));
+
+			if(tmp == NULL) perror("realloc");
+
+			t_cmd = tmp;
+		}
+		t_cmd[i++] = t;
+		t = strtok(NULL, " ");
+
+	}
+	*num_cmd = i;
+	t_cmd[i] = NULL;	
+	
+	return t_cmd;
+}
+
+void fetch_info(char *buffer){
+	char hostname[1024];
+       gethostname(hostname, sizeof(hostname));	
+		
+	char curr_dir[1024];
+	getcwd(curr_dir, sizeof(curr_dir));
+
+	strcat(buffer, getlogin());
+	strcat(buffer, "@");
+	strcat(buffer, hostname);
+	strcat(buffer, ": ");
+	strcat(buffer, curr_dir);
+	strcat(buffer, " > ");
+}
+
 int main(){
 	while(1){
 		char buffer[512];
@@ -164,9 +165,9 @@ int main(){
 		node_t *head;
 		check_pro(&head);
 
-		if(num_cmd == 0) continue;
-
-		if(strcmp(tokenized[0], "cd") == 0)
+		if(num_cmd == 0) 
+			continue;
+		else if(strcmp(tokenized[0], "cd") == 0)
 			change_dir(tokenized, num_cmd);	
 		else if(strcmp(tokenized[0], "bg") == 0)
 			background_pro(tokenized, &head);
@@ -177,6 +178,5 @@ int main(){
 		else 
 			execute_cmd(tokenized);
 	}
-
 	return 0;
 }
