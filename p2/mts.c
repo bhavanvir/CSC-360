da53bdf12c795001a43f34a5e1257ed36c49ad25
Bhavanvir Rai <bhavanvir.r@gmail.com>
2022-03-04 20:58:05 -0800

    Add files via upload

--- p2/mts.c
+++ p2/mts.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <string.h>
+#include <stdbool.h>
 
 typedef struct train_t
 {
@@ -12,12 +13,75 @@ typedef struct train_t
     int crossing_time;
 } train_t;
 
+typedef struct node_t
+{
+    int priority;
+    train_t *train_data;
+    struct node_t *next;
+} node_t;
+
+void push(node_t **head, train_t *train_data)
+{
+    node_t *front = *head;
+
+    node_t *tmp = (node_t *)malloc(sizeof(node_t));
+    tmp->train_data = train_data;
+    tmp->priority = train_data->priority;
+    tmp->next = NULL;
+
+    if (*head == NULL)
+    {
+        *head = tmp;
+        return;
+    }
+
+    if (tmp->train_data->loading_time == front->train_data->loading_time && tmp->train_data->train_number < front->train_data->train_number)
+    {
+        tmp->next = front;
+        front = tmp;
+    }
+    else
+    {
+        while (tmp->train_data->loading_time == front->next->train_data->loading_time && tmp->train_data->train_number < front->next->train_data->train_number)
+            front = front->next;
+
+        tmp->next = front->next;
+        front->next = tmp;
+    }
+}
+
+void pop(node_t **head)
+{
+    node_t *tmp = *head;
+    *head = (*head)->next;
+    free(tmp);
+}
+
+bool isEmpty(node_t **head)
+{
+    return *head == NULL;
+}
+
+pthread_mutex_t station_mutex, track_mutex;
+pthread_cond_t load_cond, cross_cond;
+
 void *load_train(void *arg)
 {
+    train_t *curr_train = arg;
+
+    pthread_mutex_lock(&station_mutex);
+
+    pthread_mutex_unlock(&station_mutex);
 }
 
 int main(int argc, char *argv[])
 {
+    pthread_mutex_init(&station_mutex, NULL);
+    pthread_mutex_init(&track_mutex, NULL);
+
+    pthread_cond_init(&load_cond, NULL);
+    pthread_cond_init(&cross_cond, NULL);
+
     if (argv[1] == NULL)
     {
         printf("Expected input file of the form: *.txt\n");
@@ -37,6 +101,8 @@ int main(int argc, char *argv[])
 
     train_t *train = malloc(total_trains * sizeof(*train));
 
+    pthread_t train_thread[total_trains];
+
     char col1[1], col2[3], col3[3];
     int i = 0;
     while (fscanf(fp, "%s %s %s", col1, col2, col3) != EOF)
@@ -49,8 +115,6 @@ int main(int argc, char *argv[])
         i++;
     }
     fclose(fp);
-
-    pthread_t train_thread[total_trains];
 
     for (int i = 0; i < total_trains; i++)
     {
