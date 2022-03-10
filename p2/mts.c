a67e5398db29bcb0138ef4e63af6ca20eda3f438
Bhavanvir Rai <bhavanvir.r@gmail.com>
2022-03-09 20:18:57 -0800

    Add files via upload

--- p2/mts.c
+++ p2/mts.c
@@ -12,6 +12,8 @@ double accum;
 pthread_mutex_t station_mutex, track_mutex;
 pthread_cond_t load_cond, cross_cond;
 
+int total_trains, sent_east, sent_west = 0;
+
 typedef struct train_t
 {
     int train_number;
@@ -90,16 +92,16 @@ void print_output(train_t *curr_train, char *print_flag)
     else if (strcmp(print_flag, "ON") == 0)
         printf("%02d:%02d:%04.1f Train %2d is ON on the main track going %4s\n", hours, minutes, accum, curr_train->train_number, direction);
     else if (strcmp(print_flag, "OFF") == 0)
-        printf("%02d:%02d:%04.1f Train %2d is OFF the main track going %4s\n", hours, minutes, accum, curr_train->train_number, direction);
+        printf("%02d:%02d:%04.1f Train %2d is OFF the main track after going %4s\n", hours, minutes, accum, curr_train->train_number, direction);
 }
 
 void *load_train(void *arg)
 {
     train_t *curr_train = arg;
 
-    usleep(curr_train->loading_time * 100000);
-
     char *direction = curr_train->direction == 'E' || 'e' ? "East" : "West";
+
+    usleep(curr_train->loading_time * 100000);
 
     print_output(curr_train, "READY");
 
@@ -108,18 +110,65 @@ void *load_train(void *arg)
     pthread_mutex_unlock(&station_mutex);
 
     pthread_cond_signal(&load_cond);
-    while (curr_train->ready != true);
+    while (curr_train->ready != true)
+        ;
 
     print_output(curr_train, "ON");
     usleep(curr_train->crossing_time * 100000);
     print_output(curr_train, "OFF");
+    total_trains--;
 
     pthread_cond_signal(&cross_cond);
 }
 
+void send_train()
+{
+    char direction[1];
+    while (total_trains > 0)
+    {
+        pthread_mutex_lock(&track_mutex);
+
+        if (isEmpty(&station_east) == true && isEmpty(&station_west) == true)
+            pthread_cond_wait(&load_cond, &track_mutex);
+
+        if (isEmpty(&station_east) == false && isEmpty(&station_west) == false)
+        {
+            sent_east == 4 ? strncpy(direction, "W", 1) : strncpy(direction, "E", 1);
+            sent_west == 4 ? strncpy(direction, "E", 1) : strncpy(direction, "E", 1);
+            sent_east = sent_east == 4 ? 0 : sent_east;
+            sent_west = sent_west == 4 ? 0 : sent_west;
+            station_east->priority > station_west->priority ? strncpy(direction, "E", 1) : strncpy(direction, "W", 1);
+        }
+        else if (isEmpty(&station_east) == false && isEmpty(&station_west) == true)
+            strncpy(direction, "E", 1);
+        else if (isEmpty(&station_east) == true && isEmpty(&station_west) == false)
+            strncpy(direction, "W", 1);
+
+        if (strcmp(direction, "E") == 0)
+        {
+            sent_east++;
+            sent_west = 0;
+            station_east->train_data->ready = true;
+            pthread_mutex_lock(&station_mutex);
+            pop(&station_east);
+            pthread_mutex_unlock(&station_mutex);
+        }
+        else if (strcmp(direction, "W") == 0)
+        {
+            sent_west++;
+            sent_east = 0;
+            station_west->train_data->ready = true;
+            pthread_mutex_lock(&station_mutex);
+            pop(&station_west);
+            pthread_mutex_unlock(&station_mutex);
+        }
+        pthread_cond_wait(&cross_cond, &track_mutex);
+        pthread_mutex_unlock(&track_mutex);
+    }
+}
+
 int main(int argc, char *argv[])
 {
-    /* INPUT INTEGRITY CHECK */
     if (argv[1] == NULL)
     {
         printf("Expected input file of the form: *.txt\n");
@@ -131,12 +180,9 @@ int main(int argc, char *argv[])
     if (fp == NULL)
         perror("Error at fp");
 
-    /* TIMER START */
     if (clock_gettime(CLOCK_REALTIME, &start) == -1)
         perror("Error at clock_gettime with start");
 
-    /* TRAIN ALLOCATION */
-    int total_trains = 0;
     for (int c = getc(fp); c != EOF; c = getc(fp))
         if (c == '\n')
             total_trains++;
@@ -158,7 +204,6 @@ int main(int argc, char *argv[])
     }
     fclose(fp);
 
-    /* PTHREAD HANDLING */
     pthread_mutex_init(&station_mutex, NULL);
     pthread_mutex_init(&track_mutex, NULL);
 
@@ -170,11 +215,10 @@ int main(int argc, char *argv[])
     for (int i = 0; i < total_trains; i++)
     {
         if (pthread_create(&train_thread[i], NULL, &load_train, (void *)&train[i]) != 0)
-        {
-            fprintf(stderr, "Error at train_thread[%d]", i);
-            perror("");
-        }
+            perror("Error at train_thread[i]");
     }
+
+    send_train();
 
     for (int i = 0; i < total_trains; i++)
         pthread_join(train_thread[i], NULL);
