e98de441210d0bd3a52e83fae884122cfbe948e4
Bhavanvir Rai <bhavanvir.r@gmail.com>
2022-04-03 11:27:24 -0700

    Add files via upload

--- p3/parts.c
+++ p3/parts.c
@@ -51,9 +51,9 @@ struct __attribute__((__packed__)) dir_entry_t
 
 void diskinfo(int argc, char *argv[])
 {
-    if (argc < 2)
+    if (argc != 2)
     {
-        printf("Error: expected a disk image in the form *.img");
+        printf("Expected: ./diskinfo <disk image>\n");
         exit(1);
     }
 
@@ -71,19 +71,24 @@ void diskinfo(int argc, char *argv[])
     struct superblock_t *sb;
     sb = (struct superblock_t *)address;
 
+    // Initialize the start and end location variables to their corresponding values in the FAT table
     int start = ntohl(sb->fat_start_block) * htons(sb->block_size);
     int end = ntohl(sb->fat_block_count) * htons(sb->block_size);
+    // Initialize the number of free, reserved and allocated blocks to 0 initially
     int free, reserved, allocated = 0;
 
+    // Loop from the start of the FAT black to the end, incrementing by 4 each time
     for (int i = start; i < start + end; i += 4)
     {
         int val = 0;
         memcpy(&val, address + i, 4);
         val = htonl(val);
+        // Depending on the value at the address + i space, increment the appropriate variable
         val == 0 ? free++ : val == 1 ? reserved++
                                      : allocated++;
     }
 
+    // Print the appropriate super block information
     printf("Super block information:\n");
     printf("Block size: %d\n", htons(sb->block_size));
     printf("Block count: %d\n", ntohl(sb->file_system_block_count));
@@ -92,20 +97,22 @@ void diskinfo(int argc, char *argv[])
     printf("Root directory start: %d\n", ntohl(sb->root_dir_start_block));
     printf("Root directory blocks: %d\n\n", ntohl(sb->root_dir_block_count));
 
+    // Print the appropriate FAT information
     printf("FAT information:\n");
     printf("Free Blocks: %d\n", free);
     printf("Reserved Blocks: %d\n", reserved);
     printf("Allocated Blocks: %d\n", allocated);
 
+    // Delete the mappings for the address range and close the file
     munmap(address, buffer.st_size);
     close(fd);
 }
 
 void disklist(int argc, char **argv)
 {
-    if (argc < 3 || argc > 3)
+    if (argc != 3)
     {
-        printf("Error: expected a disk image directory\n");
+        printf("Expected: ./disklist <disk image> /\n");
         exit(1);
     }
 
@@ -126,17 +133,21 @@ void disklist(int argc, char **argv)
     int size = htons(sb->block_size);
     int start = ntohl(sb->root_dir_start_block) * size;
 
+    // Create a new variable corresponding to the rootblock of the directory entry
     struct dir_entry_t *rb;
 
+    // Loop from the start of the block to the end, incrementing by 64 each time
     for (int i = start; i < start + size; i += 64)
     {
+        // Set the rootblock variable to the value of the memory location at address + i
         rb = (struct dir_entry_t *)(address + i);
+        // If the size of the rootblock is 0, skip the entry entirely
         if (ntohl(rb->size) == 0)
             continue;
+        // Print the correct information pertaining to the disk image
         printf("%c %10d %30s %4d/%02d/%02d %02d:%02d:%02d\n", rb->status == 3 ? 'F' : 'D', ntohl(rb->size), rb->filename, htons(rb->modify_time.year),
                rb->modify_time.month, rb->modify_time.day, rb->modify_time.hour, rb->modify_time.minute, rb->modify_time.second);
     }
-    return;
 
     munmap(address, buffer.st_size);
     close(fd);
@@ -144,9 +155,9 @@ void disklist(int argc, char **argv)
 
 void diskget(int argc, char *argv[])
 {
-    if (argv[2] == NULL || argv[3] == NULL)
+    if (argc != 4)
     {
-        printf("Error: too few arguments\n");
+        printf("Expected: ./diskget <disk image> /<disk file> <local file>\n");
         exit(1);
     }
 
@@ -174,33 +185,45 @@ void diskget(int argc, char *argv[])
     int start = htonl(sb->root_dir_start_block) * size;
     void *file = mmap(NULL, htonl(sb->file_system_block_count) * size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
 
+    // Initialize variables for the file size, status and file_found boolean
     int fs;
-    char stat;
+    char status;
     bool file_found = false;
 
     int idx = 0;
+    // Loop from the initial 0th index to the start of the block, incrementing by 64 each time
     for (int i = 0; i < start; i += 64)
     {
-        memcpy(&stat, file + start + i, 1);
+        // Copy the status of the address space into the status varialbe
+        memcpy(&status, file + start + i, 1);
 
+        // Create a temporary buffer to copy information from certain address spaces
         char tmp[1000];
         memcpy(&fs, file + start + i + 9, 4);
         fs = ntohl(fs);
-        for (int name_count = 27; name_count < 54; name_count++)
+        for (int j = 27; j < 54; j++)
         {
-            memcpy(&stat, file + start + i + name_count, 1);
-            tmp[idx] = stat;
+            // Copy the status of the current address space into the status variable each loop
+            memcpy(&status, file + start + i + j, 1);
+            // Set the current index value to the status variable
+            tmp[idx] = status;
+            // Increment the current working index
             idx++;
         }
         idx = 0;
 
-        if (strcmp(tmp, file_data) == 0)
+        // If the file_data and temporary variable contain the same information then continue
+        if (strcmp(tmp, file_data) == false)
         {
+            // The file has been found
             file_found = true;
 
+            // Open the appopriate file on the user's local machine in writing mode
             FILE *out = fopen(argv[3], "w");
 
+            // Copy data from the appropriate location in memory and store it within the file size variable
             memcpy(&fs, file + start + i + 5, 4);
+            // Convert the unsigned integer to nhost byte order, after each memory copy operation
             fs = ntohl(fs);
             memcpy(&fs, file + start + i + 9, 4);
             fs = ntohl(fs);
@@ -209,17 +232,21 @@ void diskget(int argc, char *argv[])
             fs = ntohl(fs);
 
             char file_buffer;
+            // While the file size variable doesn't result in error, particularly reaching the end of the file, continue looping
             while (fs != -1)
             {
+                // If the current file size is greater than the total file size continue writing to the appropriate output file
                 if (file_size > size)
                 {
-                    file_size -= size;
                     for (int i = 0; i < size; i++)
                     {
                         memcpy(&file_buffer, file + size * fs + i, 1);
                         fwrite(&file_buffer, sizeof(char), 1, out);
                     }
+                    // Decrement the current file size by the total file size, to get the remaining total
+                    file_size -= size;
                 }
+                // If not, continue writing to the appropriate output file
                 else
                 {
                     for (int i = 0; i < file_size; i++)
@@ -229,11 +256,15 @@ void diskget(int argc, char *argv[])
                     }
                 }
 
+                // Copy data from the final address space in the block to the file size vriable
                 memcpy(&fs, file + htonl(sb->fat_start_block) * size + fs * 4, 4);
                 fs = ntohl(fs);
             }
+
+            fclose(out);
         }
     }
+    // If the file is not found return an error; else notify the user of successful completion
     if (file_found == false)
     {
         printf("Error: file not found\n");
@@ -248,11 +279,185 @@ void diskget(int argc, char *argv[])
 
 void diskput(int argc, char *argv[])
 {
+    if (argc != 4)
+    {
+        printf("Expected: ./diskput <disk image> <local file> /<diskfile>\n");
+        exit(1);
+    }
+
+    int fd = open(argv[1], O_RDWR);
+    if (fd == -1)
+        perror("Error at fd");
+
+    struct stat buffer_tp;
+    fstat(fd, &buffer_tp);
+
+    int f_tu = open(argv[2], O_RDWR);
+    if (f_tu == -1)
+    {
+        printf("Error: file not found\n");
+        exit(1);
+    }
+
+    struct stat buffer_tu;
+    fstat(f_tu, &buffer_tu);
+
+    char *file_name = argv[3];
+    memmove(&file_name[0], &file_name[1], strlen(file_name));
+    int file_size = buffer_tu.st_size;
+
+    FILE *fp;
+    fp = fopen(argv[2], "r");
+
+    void *address = mmap(NULL, buffer_tp.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    if (address == (void *)-1)
+        perror("Error at address");
+
+    struct superblock_t *sb;
+    sb = (struct superblock_t *)address;
+
+    int size = htons(sb->block_size);
+    int start = ntohl(sb->fat_start_block) * size;
+    int block = ntohl(sb->root_dir_start_block) * size;
+    int end = (ntohl(sb->fat_start_block) + ntohl(sb->fat_block_count)) * size;
+
+    // Create a variable declaring the needed file size by computing a modulo operation on the remnant value, and incrementing appropriately
+    int need = file_size / size;
+    file_size % size != 0 ? need++ : need;
+
+    // Declare a variable with the amount of memory used
+    int used = 0;
+    int memory_idx = 0, starting_idx = 0;
+    char tmp[1000];
+    // Loop from the beginning of the block to the end, incrementing by 4 each time
+    for (int i = start; i < end; i += 4)
+    {
+        int idx = 0;
+        // Copy the value of the address space into the current index value
+        memcpy(&idx, address + i, 4);
+        // Convert the unsigned integer to network byte order
+        idx = htonl(idx);
+        if (idx == 0)
+        {
+            // If no memory has been currently used, update the starting index and read from the appropriate local file
+            if (used == 0)
+            {
+                starting_idx = (i - start) / 4;
+                used++;
+
+                fread(tmp, size, 1, fp);
+                memcpy(address + (starting_idx * size), &tmp, size);
+            }
+            // Update the memory index location
+            memory_idx = (i - start) / 4;
+
+            // Read from the local file and copy the apportiate data from the location in memory
+            fread(tmp, size, 1, fp);
+            memcpy(address + (memory_idx * size), &tmp, size);
+
+            // Convert the memory index variable from an unsigned integer to network byte order
+            memory_idx = htonl(memory_idx);
+            memcpy(address + i, &memory_idx, 4);
+
+            // Increment the amount of memory used
+            used++;
+        }
+    }
+
+    // Set the status of the file added to the disk image as "F"
+    int status = 3;
+    // Loop from the beginning of the file to the end, incrementing by 64 each time
+    for (int i = block; i < block + size; i += 64)
+    {
+        int idx = 0;
+        // Copy the data from the current address space into the index value
+        memcpy(&idx, address + i, 1);
+
+        if (idx == 0)
+        {
+            // Get the value of the local time zone
+            time_t rawtime;
+            time(&rawtime);
+            // From time.h create a structure used to hold the time and date
+            struct tm *info = localtime(&rawtime);
+
+            // Create a buffer that holds the time information
+            char buffer_time[10];
+            int time;
+
+            memcpy(address + i, &status, 1);
+
+            // Reinitialize the starting index, needed memory, and file size variables
+            starting_idx = ntohl(starting_idx);
+            memcpy(address + i + 1, &starting_idx, 4);
+
+            need = htonl(need);
+            memcpy(address + i + 5, &need, 4);
+
+            file_size = htonl(file_size);
+            memcpy(address + i + 9, &file_size, 4);
+
+            // Get the current year
+            strftime(buffer_time, sizeof(buffer_time), "%Y", info);
+            sscanf(buffer_time, "%d", &time);
+            time = htons(time);
+            memcpy(address + i + 20, &time, 2);
+            memcpy(address + i + 13, &time, 2);
+
+            // Get the current month
+            strftime(buffer_time, sizeof(buffer_time), "%m", info);
+            sscanf(buffer_time, "%d", &time);
+
+            memcpy(address + i + 22, &time, 1);
+            memcpy(address + i + 15, &time, 1);
+
+            // Get the current day
+            strftime(buffer_time, sizeof(buffer_time), "%d", info);
+            sscanf(buffer_time, "%d", &time);
+            memcpy(address + i + 23, &time, 1);
+            memcpy(address + i + 16, &time, 1);
+
+            // Get the current hour
+            strftime(buffer_time, sizeof(buffer_time), "%H", info);
+            sscanf(buffer_time, "%d", &time);
+            memcpy(address + i + 24, &time, 1);
+            memcpy(address + i + 17, &time, 1);
+
+            // Get the current month
+            strftime(buffer_time, sizeof(buffer_time), "%M", info);
+            sscanf(buffer_time, "%d", &time);
+            memcpy(address + i + 25, &time, 1);
+            memcpy(address + i + 18, &time, 1);
+
+            // Get the current seconds
+            strftime(buffer_time, sizeof(buffer_time), "%S", info);
+            sscanf(buffer_time, "%d", &time);
+            memcpy(address + i + 26, &time, 1);
+            memcpy(address + i + 19, &time, 1);
+
+            // Set the desired file name in the disk image
+            strncpy(address + i + 27, file_name, 31);
+
+            break;
+        }
+    }
+    // Notify the user of what the file was placed as in the disk image
+    printf("Success: placed %s as %s in %s\n", argv[2], argv[3], argv[1]);
+
+    munmap(address, buffer_tp.st_size);
+    close(fd);
+    close(f_tu);
+    fclose(fp);
 }
 
 void diskfix(int argc, char *argv[])
 {
-    printf("Not completed\n");
+    if (argc != 2)
+    {
+        printf("Expected: ./diskfix <disk image>\n");
+        exit(1);
+    }
+    printf("Not attempted 😭\n");
 }
 
 int main(int argc, char *argv[])
